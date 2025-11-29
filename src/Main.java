313adbb758ec1db6f89c0edd06360670ff0da7c2
Gaïa Lehart <gaia.lehart@gmail.com>
2025-11-29 13:51:16 +0100

    main changé

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
+import java.util.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, Projet Collecte !");
+
+    public static void main(String[] args) throws Exception {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Plan de la ville fictive");
+        Graphe graphe = LecteurOSM.chargerFichier("ville.osm");
+        System.out.println("Graphe chargé : " + graphe.getNoeuds().size() + " noeuds");
+
+        System.out.println("\ntournée des encombrants (via plus court chemin)");
+        System.out.print("ID du dépôt : ");
+        String idDepot = sc.nextLine();
+
+        System.out.print("ID de la maison à visiter : ");
+        String idMaison = sc.nextLine();
+
+        Noeud depot = graphe.getNoeud(idDepot);
+        Noeud maison = graphe.getNoeud(idMaison);
+
+        if (depot == null || maison == null) {
+            System.out.println("ID invalide.");
+            return;
+        }
+
+        List<Noeud> trajet = Dijkstra.plusCourtChemin(graphe, depot, maison);
+
+        System.out.println("\nItinéraire optimal :");
+        for (Noeud n : trajet) {
+            System.out.println(" → " + n.getId());
+        }
     }
 }
