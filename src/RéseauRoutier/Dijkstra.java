33eb6b9309a686eddddc56982a6840171dccb26f
KirineC <kirine.cai@edu.Ece.fr>
2025-11-29 15:43:58 +0100

    format txt

--- src/RéseauRoutier/Dijkstra.java
+++ src/RéseauRoutier/Dijkstra.java
@@ -8,41 +8,43 @@ public class Dijkstra {
 
         Map<Noeud, Double> distance = new HashMap<>();
         Map<Noeud, Noeud> precedent = new HashMap<>();
-        PriorityQueue<Noeud> file = new PriorityQueue<>(Comparator.comparing(distance::get));
+        Set<Noeud> visited = new HashSet<>();
+        PriorityQueue<Noeud> queue = new PriorityQueue<>(Comparator.comparing(distance::get));
 
+        // Initialisation
         for (Noeud n : graphe.getNoeuds().values()) {
             distance.put(n, Double.MAX_VALUE);
             precedent.put(n, null);
         }
-
         distance.put(depart, 0.0);
-        file.add(depart);
+        queue.add(depart);
 
-        while (!file.isEmpty()) {
-            Noeud courant = file.poll();
+        while (!queue.isEmpty()) {
+            Noeud courant = queue.poll();
+            if (!visited.add(courant)) continue; // ignore si déjà visité
 
             if (courant.equals(arrivee)) break;
 
-            for (Arete a : graphe.getAdjacence().get(courant)) {
+            List<Arete> voisins = graphe.getAdjacence().getOrDefault(courant, new ArrayList<>());
+            for (Arete a : voisins) {
                 Noeud voisin = a.getArrivee();
                 double nouvelleDist = distance.get(courant) + a.getDistance();
 
                 if (nouvelleDist < distance.get(voisin)) {
                     distance.put(voisin, nouvelleDist);
                     precedent.put(voisin, courant);
-                    file.add(voisin);
+                    queue.add(voisin); // on ajoute simplement, la PriorityQueue sera triée au prochain poll
                 }
             }
         }
 
+        // Reconstruction du chemin
         List<Noeud> chemin = new ArrayList<>();
         Noeud actuel = arrivee;
-
         while (actuel != null) {
             chemin.add(actuel);
             actuel = precedent.get(actuel);
         }
-
         Collections.reverse(chemin);
         return chemin;
     }
